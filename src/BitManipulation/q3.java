2f63fc4d8382e5c900f983fc86c9daa254dd6cd8
Shaurya <as.shauryagupta@gmail.com>
2023-01-09 21:31:51 +0530

    Bit Manipulation q3.java added

---
+++ src/BitManipulation/q3.java
@@ -0,0 +1,26 @@
+package BitManipulation;
+//find ith bit a number
+//Example-100001101  ith position from last is 2
+//output is 0
+
+import java.util.Scanner;
+
+public class q3 {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=in.nextInt();
+        System.out.println("enter the ith position");
+        int pos=in.nextInt();
+        int k,ans=n;
+        for(k=0;k<pos;k++){
+            ans=ans>>1;
+        }
+        String bin=Integer.toBinaryString(n);
+        System.out.println("Binary Value of a number is :"+ bin);
+
+        System.out.println("Answer is");
+        System.out.println(ans&1);
+
+    }
+}
