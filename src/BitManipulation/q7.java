a7dfc96fc3ae80ef7e7aeb1b031a9e048cb00e2a
Shaurya <as.shauryagupta@gmail.com>
2023-01-10 17:04:25 +0530

    Bit Manipulation q7.java updated

--- src/BitManipulation/q7.java
+++ src/BitManipulation/q7.java
@@ -10,12 +10,15 @@ public class q7 {
         System.out.println("enter the number");
         int n=in.nextInt();
         int c=n,ans=0,i,count=1;
-        while(c!=0){
+        int value=(int)(Math.log(n)/Math.log(2))+1;
+        System.out.println(value);
+        while(count<=value){
             ans=ans+(c&1)*(int)Math.pow(5,count);
             c=c>>1;
             count++;
         }
         System.out.println("number is:"+Integer.toBinaryString(n));
         System.out.println("magic number is:"+ans);
+
     }
 }
