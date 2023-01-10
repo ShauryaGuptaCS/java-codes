5b38b4bfdace704080f9aa1ba7c44a52cdf7824c
Shaurya <as.shauryagupta@gmail.com>
2023-01-10 17:30:37 +0530

    Bit Manipulation q8.java updated

--- src/BitManipulation/q8.java
+++ src/BitManipulation/q8.java
@@ -8,15 +8,7 @@ public class q8 {
         Scanner in=new Scanner(System.in);
         System.out.println("Enter the number");
         int n=in.nextInt();
-        int value=(int)(Math.log(n)/Math.log(2))+1;
-        int c=n,count=0,i=1;
-        while(i<=value){
-            if((c&1)==1)
-                count+=1;
-            i+=1;
-            c=c>>1;
-        }
-        if(count==1)
+        if((n&(n-1))==0)
             System.out.println("It is power of two");
         else
             System.out.println("It is not power of two");
