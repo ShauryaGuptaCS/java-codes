1ab18d36ab78c7a5e56caddf7f46a942e554072e
Shaurya <as.shauryagupta@gmail.com>
2023-01-10 16:53:20 +0530

    Bit Manipulation q7.java added

---
+++ src/BitManipulation/q7.java
@@ -0,0 +1,21 @@
+package BitManipulation;
+
+import java.util.Scanner;
+
+//find nth magic number
+//1=001(5^3*0 +5^2*0+ 5^1*0)
+public class q7 {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=in.nextInt();
+        int c=n,ans=0,i,count=1;
+        while(c!=0){
+            ans=ans+(c&1)*(int)Math.pow(5,count);
+            c=c>>1;
+            count++;
+        }
+        System.out.println("number is:"+Integer.toBinaryString(n));
+        System.out.println("magic number is:"+ans);
+    }
+}
